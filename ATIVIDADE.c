a32543a6e899eab6633a5e22df0d1ccb95176693
Mateus Campos <mateusrcampos@gmail.com>
2022-11-03 18:30:42 -0300

    New updates Atividade 1

--- ATIVIDADE.c
+++ ATIVIDADE.c
@@ -1,104 +1,97 @@
-/* Armazene informações relacionadas à veículos, totalizando, no máximo, 10 veículos.
-Para cada veículo, deverão ser fornecidos os seguintes dados: 
-- Marca do veículo 
+/*Armazene informações relacionadas à veículos, totalizando, no máximo, 10 veículos.
+Para cada veículo, deverão ser fornecidos os seguintes dados:
+- Marca do veículo
 - modelo do veículo
-- Ano de fabricação 
+- Ano de fabricação
 - Placa (formato XXX-YYYY; onde X denota as letras e Y denota os números da placa).
 
-O sistema deverá oferecer, ao usuário, as seguintes funcionalidades: 
-- Listar os veículos cadastrados; 
-- Inserir um novo veículo; - Listar os veículos filtrando-se por ano de fabricação; 
-- Listar os veículos com o ano de fabricação acima de um certo valor especificado pelo usuário. 
-- Listar os veículos filtrando-se pelo modelo. 
-O sistema deverá armazenar os veículos ordenados pelo ano de fabricação, ou seja, ao inserir um novo veículo, 
+O sistema deverá oferecer, ao usuário, as seguintes funcionalidades:
+- Listar os veículos cadastrados;
+- Inserir um novo veículo;
+- Listar os veículos filtrando-se por ano de fabricação;
+- Listar os veículos com o ano de fabricação acima de um certo valor especificado pelo usuário.
+- Listar os veículos filtrando-se pelo modelo.
+O sistema deverá armazenar os veículos ordenados pelo ano de fabricação, ou seja, ao inserir um novo veículo,
 este deve ser inserido em ordem crescente de ano de fabricação.*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct
+typedef struct Veiculos
 {
-    int modeloCad, AnoFabCad;
-    char marcaCad[100], placaCad[100];
+    char modelo[20];
+    char marca[20];
+    char placa[20];
+    int ano;
 
-}Veiculo;
+} Veiculos;
 
-Veiculo Carros;
+Veiculos CarrosCad[10];
+Veiculos CarrosNovos;
 
 void menu();
-void inicializar();
 void imprimir();
-void cadastrar();
 
 int main()
 {
-    int modelo[10] = {2011, 2012, 2012, 2014, 2015, 2016, 2017, 2018, 2021, 2022};
-    int AnoFab[10] = {2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2020, 2021};
-    char marca[10] = {"Mercedes", "Hyundi", "Ferrari", "Wolksvagen", "Chevrolet", "KIA", "Masserati", "BMW", "Audi", "Jeep"};
-    char placa[10] = {"AAA-0000", "BBB-1111", "CCC-2222", "DDD-3333", "EEE-4444", "FFF-5555", "GGG-6666", "HHH-7777", "III-8888", "JJJ-9999"};
-    for (int i = 0; i < 10; i++)
-    {
-        printf("\nMarca do carro: %c\nModelo do Carro: %d\nAno de fabricacao: %d\nPlaca: %c\n", marca[i], modelo[i], AnoFab[i], placa[i]);
-    }
     menu();
     return 0;
 }
 
 void menu()
 {
-    int opcao;
+    int escolha, i=0;
     do
     {
-        printf("\n Opcoes: ");
-        printf("\n 1. Inicializr a Estrutura");
-        printf("\n 2. Imprimir a Estrutura");
-        printf("\n 3. Cadastrar Carro");
-        printf("\n 4. Para sair. \n");
-        scanf("%d%*c", &opcao);
-        switch (opcao)
+        printf("\n============ SISTEMA VEICULOS ==============\n");
+        printf("=================== MENU =====================\n");
+        printf("1. Lista(s) do(s) Veiculo(s) cadastrado(s): ");
+        printf("\n2. Cadastro de veiculo(s): ");
+        printf("\n3. Para buscar o(s) veiculo(s) acima do ano de fabricacao especificado: ");
+        printf("\n4. Para buscar o(s) modelo(s) do(s) veiculo(s): ");
+        printf("\n5. Para sair do menu: \n");
+        scanf("%d", &escolha);
+        switch (escolha)
         {
         case 1:
-            inicializar();
-            break;
+            if (i == 0)
+            {
+                printf("========= NENHUM VEICULO CADASTRADO ===========\n");
+            }
+            else
+            {
+                printf("\n============ LISTA DE VEICULO ==============\n");
+                for (int i = 0; i < 1; i++)
+                printf("Modelo do Veiculo cadastrado %s", CarrosCad[i].modelo);
+                printf("Marca do Veiculo cadastrado: %s", CarrosCad[i].marca);
+                printf("Placa do Veiculo cadastrado: %s", CarrosCad[i].placa);
+                printf("Ano do Veículo cadastrado: %d", CarrosCad[i].ano);
+            }
+        break;
         case 2:
-            imprimir();
-        case 3:
-            cadastrar();
-        break;
+        if(i == 15)
+        {
+            printf("=== Valor maximo de 15 carros cadastrados ===");
+        }else if(i == 10){
+            printf("=== Valor de 10 carros cadastrados ===\n=== Voce pode inserir mais 5 veiculos\nInseri escolha 2 no menu");
+        }else
+        {
+            printf("\n\n============ CADASTRO DE VEICULO ==============\n");
+            printf("\nDigite o Modelo do veiculo: ");
+            scanf("%100[^\n]%*c", &CarrosCad[i].modelo);
+            printf("\nDigite a marca do veiculo: ");
+            scanf("%100[^\n]%*c", &CarrosCad[i].marca);
+            printf("\nDigite a placa do veiculo (AAA-1111): ");
+            scanf("%10[^\n]%*c", &CarrosCad[i].placa);
+            printf("\nDigite o ano do veiculo: ");
+            scanf("%d", &CarrosCad[i].ano);
         default:
-        printf("\n Opcao nao existente. ");
-        break;
+            break;
         }
-    } while (opcao!=4);
-}
+    }
+}while (escolha != 5);
 
-void inicializar()
-{
-    strcpy(Carros.marcaCad, "NULL");
-    Carros.modeloCad = 0;
-    Carros.AnoFabCad = 0;
-    strcpy(Carros.placaCad, "NULL");
-    
-}
-
-void cadastrar()
-{
-    printf("Digite a Marca do Carro: ");
-    scanf("%s", &Carros.marcaCad);
-    printf("Digite o Modelo do Carro: ");
-    scanf("%d", &Carros.modeloCad);
-    printf("Digite o Ano de fabricacao do Carro: ");
-    scanf("%d", &Carros.AnoFabCad);
-    printf("Digite a placa do Carro: ");
-    scanf("%s", &Carros.placaCad);
-}
-
-void imprimir()
-{
-    printf("\nA Marca do Carro: ");
-    printf("\nO Modelo do Carro: ");
-    printf("\nO Ano de Fabricacao do Carro: ");
-    printf("\nA placa do Carro: ");
-    printf("\n\n");
+getchar();
+return 0;
 }
