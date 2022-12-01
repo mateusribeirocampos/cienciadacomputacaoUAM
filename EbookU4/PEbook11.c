3bf0b21b8ccdc8a74cfa6e997c5464ec0496306e
Mateus Campos <mateusrcampos@gmail.com>
2022-12-01 10:14:40 -0300

    Atualização completa do Ebbok - código fonte para arquivos

--- EbookU4/PEbook11.c
+++ EbookU4/PEbook11.c
@@ -34,12 +34,27 @@ int AlteraRegistro(long pos, TPessoa pessoa, FILE *arq)
     }
     return 0;
 }
+
+int InsereNovoRegistro(TPessoa pessoa, FILE *arq)
+{
+    long pos;
+    TPessoa p;
+    pos = LocalizaRegistro("00000000000", &p, arq);
+    if (pos != -1)
+        return (AlteraRegistro(pos, pessoa, arq));
+    fseek(arq, 0, SEEK_END);
+    if (!fwrite(&pessoa, sizeof(TPessoa), 1, arq))
+        ;
+    return -1;
+    return 0;
+}
+
 int main()
 {
     FILE *arq;
     TPessoa pessoa;
     long pos;
-    char cpf[12], resp, del;
+    char cpf[12], resp, del, inser;
     int ret;
     arq = fopen("Registros.dat", "r+");
     if (arq == NULL)
@@ -64,40 +79,57 @@ int main()
         scanf("%c", &resp);
         resp = toupper(resp);
     } while ((resp != 'S') && (resp != 'N'));
-    if(resp == 'N')
+    if (resp == 'N')
     {
         printf("Digite a nova idade: ");
         scanf("%d", &pessoa.idade);
-        ret = AlteraRegistro(pos,pessoa, arq);
-        if(ret == -1)
-        printf("Erro de atualizacao do registro.\n");
+        ret = AlteraRegistro(pos, pessoa, arq);
+        if (ret == -1)
+            printf("Erro de atualizacao do registro.\n");
     }
     printf("Testando alteracao . . . \n");
     fseek(arq, pos, SEEK_SET);
     fread(&pessoa, sizeof(TPessoa), 1, arq);
     printf("CPF = %s idade = %d\n", pessoa.cpf, pessoa.idade);
-    
+
     do
     {
         printf("Remover o registro <s> <n>\n");
         scanf("%c", &del);
         del = toupper(del);
     } while ((del != 'S') && (del != 'N'));
-    if(resp == 'S')
+    if (resp == 'S')
     {
         strcpy(pessoa.cpf, "00000000000");
         pessoa.idade = 0;
         del = AlteraRegistro(pos, pessoa, arq);
-        if(ret == -1)
-        printf("Erro de atualizacao do registro.\n");
+        if (ret == -1)
+            printf("Erro de atualizacao do registro.\n");
     }
     printf("Testando alteracao . . . \n");
     fseek(arq, pos, SEEK_SET);
     fread(&pessoa, sizeof(TPessoa), 1, arq);
     printf("CPF = %s idade = %d\n", pessoa.cpf, pessoa.idade);
 
+    do
+    {
+        printf("Inserir registro <s> <n>\n");
+        scanf("%c", &inser);
+        inser = toupper(inser);
+    } while ((inser != 'S') && (inser != 'N'));
+    if (resp == 'S')
+    {
+        printf("\nDigite o novo CPF: ");
+        scanf("%s", pessoa.cpf);
+        inser = InsereNovoRegistro(pessoa, arq);
+        if (ret == -1)
+            printf("Erro de atualizacao do registro.\n");
+    }
+    printf("Testando alteracao . . . \n");
+    fseek(arq, pos, SEEK_SET);
+    fread(&pessoa, sizeof(TPessoa), 1, arq);
+    printf("CPF = %s idade = %d\n", pessoa.cpf, pessoa.idade);
 
-    
     fclose(arq);
     return 0;
 }
