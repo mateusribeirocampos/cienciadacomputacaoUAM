0dd9529a129723c907f108caa9999f7350f61cfa
mateusribeirocampos <mateusrcampos@gmail.com>
2023-03-07 20:42:53 -0300

    Getter and Setter JAVA

---
+++ POO/GetterSetter/GetterSetter.java
@@ -0,0 +1,18 @@
+package GetterSetter;
+import java.util.Scanner;
+
+public class GetterSetter {
+    public static void main(String[] args) {
+        String time;
+        Futebol torcida = new Futebol();
+        Scanner texto = new Scanner(System.in);
+
+        System.out.println("Para qual time voce torce ");
+        time = texto.nextLine();
+
+        torcida.setTime(time);
+        torcida.mostraTime();
+        
+        texto.close();
+    }
+}
