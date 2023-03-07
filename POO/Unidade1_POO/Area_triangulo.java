f1f7041e60dd0cc034d80725241d751e7293bead
Mateus Campos <mateusrcampos@gmail.com>
2023-03-07 19:54:44 -0300

    huge updates

---
+++ POO/Unidade1_POO/Area_triangulo.java
@@ -0,0 +1,16 @@
+/***************************************************************************************************************************************************Crie uma aplicação que calcule a área de um triângulo. Elabore as variáveis necessárias e coloque o tipo de dado adequado para cada uma. No final, imprima a resposta. Lembre-se de que a base receberá o valor de 15 e a altura o valor de 7,5. Para calcular a área de um triângulo utilizamos a fórmula: área = (base x altura) / 2.
+***************************************************************************************************************************************************/
+
+package Unidade1_POO;
+
+import java.io.ObjectInputStream.GetField;
+
+public class Area_triangulo {
+    public static void main(String[] args) {
+        int base = 15;
+        double altura = 7.5;
+        double area = (base * altura) / 2;
+
+        System.out.println("A área do triângulo: " + area);
+    }
+}
---
+++ POO/Unidade1_POO/U1POOJAVA.java
@@ -0,0 +1,11 @@
+package Unidade1_POO;
+public class U1POOJAVA {
+    public static void main(String[] args) {
+        //tipos abstratos de dados
+        //tipo de dado: inteiro = int, tipo de variável:x e valor = 15 
+        int x = 15;
+        int y = 25;
+        int soma = x + y;
+        System.out.println("Soma = " + soma);
+    }    
+}
