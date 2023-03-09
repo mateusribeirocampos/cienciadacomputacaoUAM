88a74067f3b7adaf6ca8bb2b674ac4921add4187
Mateus Campos <mateusrcampos@gmail.com>
2023-03-09 15:54:20 -0300

    Exemplos array

---
+++ Estruturas_de_dados/Unidade1_POO/Array/IntArray.java
@@ -0,0 +1,25 @@
+
+public class IntArray {
+    public static void main(String[] args) {
+        int[][] array1 = {{1, 2, 3}, {4, 5, 6}};
+        int[][] array2 = {{1, 2,}, {3}, {4, 5, 6}};
+
+        System.out.println("Valores do Array 1 por linha: ");
+        outputArray(array1);
+
+        System.out.printf("\nValores do Array 2 por linha: \n");
+        outputArray(array2);
+    }
+
+    public static void outputArray(int[][] array) {
+
+        for(int linha = 0; linha < array.length; linha++) {
+
+            for(int coluna = 0; coluna < array[linha].length; coluna++) {
+                System.out.printf("%d ", array[linha][coluna]);
+            }
+            System.out.println();
+        }
+    }
+}
+
