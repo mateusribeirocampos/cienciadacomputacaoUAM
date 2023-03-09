286582806c846af932cdcda29ae46f67ef4f4962
mateusribeirocampos <mateusrcampos@gmail.com>
2023-03-09 16:25:03 -0300

    Atividade 1 de Estrutura de Dados concluída

--- Estruturas_de_dados/Atividade1/Escola/Alunos.java
+++ Estruturas_de_dados/Atividade1/Escola/Alunos.java
@@ -1,22 +1,22 @@
-package Atividade1.Escola;
+package Atividade1.Escola; //Declaração do pacote em que a classe está
+import java.util.Scanner; // importação da biblioteca do Java para leitura de dados do usuário
 
-import java.util.Scanner;
-
-public class Alunos {
+public class Alunos { 
     public static void main(String[] args) {
-        String[] nomeAluno = new String[25];
+        String[] nomeAluno = new String[25]; // criação do array de strings para armazenar os nomes dos alunos
+        Scanner lerNomeAlunoScanner = new Scanner(System.in); // criação do objeto Scanner para ler as entradas do usuário
 
-        Scanner lerNomeAlunoScanner = new Scanner(System.in);
-
-        for(int i = 0; i < nomeAluno.length; i++) {
-            System.out.print("Digite o nome do aluno " + (i + 1) + ": ");
-            nomeAluno[i] = lerNomeAlunoScanner.nextLine();
+        // Laço for para ler os nomes dos alunos e armazená-los no array
+        for(int i = 0; i < nomeAluno.length; i++) { 
+            System.out.print("Digite o nome do aluno " + (i + 1) + ": "); // solicitação do nome do aluno ao usuário
+            nomeAluno[i] = lerNomeAlunoScanner.nextLine(); // armazenamento do nome do aluno no array na posição i
         }
-        lerNomeAlunoScanner.close();
+        lerNomeAlunoScanner.close(); // notificação para fechar o objeto Scanner após a leitura
 
+        // Impressão dos nomes dos alunos cadastrados
         System.out.println("\nOs alunos cadastrados sao: ");
-        for(int i = 0; i < nomeAluno.length; i++) {
-            System.out.println((i + 1) + ". " + nomeAluno[i]);
+        for(int i = 0; i < nomeAluno.length; i++) { // laço for para percorrer o array e imprimir os nomes dos alunos
+            System.out.println((i + 1) + ". " + nomeAluno[i]); // impressão do nome do aluno na posição i
         }
     }
 }
--- Estruturas_de_dados/Atividade1/Escola1/Alunos1.java
+++ Estruturas_de_dados/Atividade1/Escola1/Alunos1.java
@@ -1,23 +1,31 @@
-package Atividade1.Escola1;
+package Atividade1.Escola1; //Declaração do pacote em que a classe está
+import java.util.ArrayList; //Importação da classe ArrayList
+import java.util.Scanner; //Importação da classe Scanner
 
-import java.util.Scanner; // Importa a classe Scanner para permitir entrada de dados
-import java.util.ArrayList; // Importa a classe ArrayList para permitir a criação de um array dinâmico
+public class Alunos1 { //Declaração da classe
 
-public class Alunos { 
-    public static void main(String[] args) {
-        ArrayList<String> nomeAluno = new ArrayList<String>(); // Cria um ArrayList para armazenar os nomes dos alunos
-        Scanner lerNomeAlunoScanner = new Scanner(System.in); // Cria um objeto da classe Scanner para permitir entrada de dados do usuário
+    public static void main(String[] args) { //Declaração do método principal da classe
 
-        for(int i = 0; i < 25; i++) { // Loop para ler os 25 nomes dos alunos
-            System.out.print("Digite o nome do aluno " + (i + 1) + ": "); // Exibe mensagem para o usuário inserir o nome do aluno
-            nomeAluno.add(lerNomeAlunoScanner.nextLine()); // Adiciona o nome do aluno no ArrayList
+        ArrayList<String> nomeAluno = new ArrayList<String>(); //Criação de um objeto ArrayList para armazenar os nomes dos alunos
+        Scanner lerNomeAlunoScanner = new Scanner(System.in); //Criação de um objeto Scanner para ler o nome dos alunos
+
+        //cadastro dos 25 alunos
+        for(int i = 0; i < 25; i++) { //Laço para cadastrar os 25 alunos
+            System.out.print("Digite o nome do aluno " + (i + 1) + ": "); //Exibe a mensagem para o usuário digitar o nome do aluno
+            nomeAluno.add(lerNomeAlunoScanner.nextLine()); //Adiciona o nome do aluno ao ArrayList
         }
 
-        System.out.println("\nOs alunos cadastrados sao: "); // Exibe mensagem informando que a listagem dos alunos irá ser exibida
+        //cadastro dos 5 novos alunos
+        for(int i = 0; i < 5; i++) { //Laço para cadastrar os 5 novos alunos
+            System.out.print("Digite o nome do novo aluno " + (i + 1) + ": "); //Exibe a mensagem para o usuário digitar o nome do novo aluno
+            nomeAluno.add(lerNomeAlunoScanner.nextLine()); //Adiciona o nome do novo aluno ao ArrayList
+        }
 
-        for(int i = 0; i < nomeAluno.size(); i++) { // Loop para exibir a lista de alunos cadastrados
-            System.out.println((i + 1) + ". " + nomeAluno.get(i)); // Exibe o número do aluno e o seu nome
+        lerNomeAlunoScanner.close(); //Fecha o objeto Scanner
+
+        System.out.println("\nOs alunos cadastrados sao: "); //Exibe a mensagem informando que serão exibidos os alunos cadastrados
+        for(int i = 0; i < nomeAluno.size(); i++) { //Laço para exibir os alunos cadastrados
+            System.out.println((i + 1) + ". " + nomeAluno.get(i)); //Exibe o número e o nome do aluno
         }
     }
 }
-
