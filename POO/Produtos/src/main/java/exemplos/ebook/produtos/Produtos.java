60de7335c194607b5c488b4790c7f93fdef86087
mateusribeirocampos <mateusrcampos@gmail.com>
2023-03-29 08:00:14 -0300

    projeto NetBeans

---
+++ POO/Produtos/src/main/java/exemplos/ebook/produtos/Produtos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package exemplos.ebook.produtos;
+
+/**
+ *
+ * @author Mateus Campos
+ */
+public class Produtos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
