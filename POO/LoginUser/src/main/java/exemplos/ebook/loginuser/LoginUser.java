89b4cf18c8b0a532bd5d02ecba49b73c3a1187b3
mateusribeirocampos <mateusrcampos@gmail.com>
2023-04-02 16:18:54 -0300

    Senha com vários caracteres

---
+++ POO/LoginUser/src/main/java/exemplos/ebook/loginuser/LoginUser.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package exemplos.ebook.loginuser;
+
+/**
+ *
+ * @author Mateus Campos
+ */
+public class LoginUser {
+
+    public static void main(String[] args) {
+        LoginTela loginTela = new LoginTela();
+        loginTela.setVisible(true);
+    }
+}
