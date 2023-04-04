c947930f398dabe3ed4444508d58081cf15bce19
mateusribeirocampos <mateusrcampos@gmail.com>
2023-04-04 17:33:10 -0300

    Cadastro de veículos Usando JTable, ArrayList, List e JDialog

---
+++ POO/CadastroVeiculos/src/main/java/exemplos/ytweyand/cadastroveiculos/CadastroVeiculos.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package exemplos.ytweyand.cadastroveiculos;
+
+import apresentacao.Principal;
+
+/**
+ *
+ * @author Mateus Campos
+ */
+public class CadastroVeiculos {
+
+    public static void main(String[] args) {
+        Principal principal = new Principal();
+        principal.setVisible(true);
+                
+    }
+}
